8106fd2b1f1852f52eff03acf217c692f6ba0f96
seymur-sudo <seymur2519@gmail.com>
2024-02-06 03:20:43 +0400

    starting chat feature

---
+++ pages/admin/chat/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "@/components/Admin/Layout";
+import { IoSend } from "react-icons/io5";
+import { MdEmojiEmotions } from "react-icons/md";
+
+const Chat: React.FC = () => {
+
+  return (
+    <Layout>
+      <div className="w-[98%] mx-[2%] rounded-[10px] h-screen bg-[#090909]">
+
+      </div>
+    </Layout>
+  );
+};
+
+export default Chat;
